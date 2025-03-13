9697e9feac698ed150f274dd8d623d35e1ef19f8
Maria Aguero <mariaguero745@gmail.com>
2025-03-12 20:38:14 -0600

    lab4

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,87 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import { IonicVue } from '@ionic/vue';
+import { IonicVue, alertController, AlertOptions } from "@ionic/vue";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/vue/css/core.css';
+import "@ionic/vue/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/vue/css/normalize.css';
-import '@ionic/vue/css/structure.css';
-import '@ionic/vue/css/typography.css';
+import "@ionic/vue/css/normalize.css";
+import "@ionic/vue/css/structure.css";
+import "@ionic/vue/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/vue/css/padding.css';
-import '@ionic/vue/css/float-elements.css';
-import '@ionic/vue/css/text-alignment.css';
-import '@ionic/vue/css/text-transformation.css';
-import '@ionic/vue/css/flex-utils.css';
-import '@ionic/vue/css/display.css';
+import "@ionic/vue/css/padding.css";
+import "@ionic/vue/css/float-elements.css";
+import "@ionic/vue/css/text-alignment.css";
+import "@ionic/vue/css/text-transformation.css";
+import "@ionic/vue/css/flex-utils.css";
+import "@ionic/vue/css/display.css";
 
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* @import '@ionic/vue/css/palettes/dark.always.css'; */
-/* @import '@ionic/vue/css/palettes/dark.class.css'; */
-import '@ionic/vue/css/palettes/dark.system.css';
+/* Ionic Dark Mode */
+import "@ionic/vue/css/palettes/dark.system.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
-const app = createApp(App)
-  .use(IonicVue)
-  .use(router);
+
+import OneSignal, { NotificationClickEvent, NotificationWillDisplayEvent } from "onesignal-cordova-plugin";
+
+const app = createApp(App).use(IonicVue).use(router);
 
 router.isReady().then(() => {
-  app.mount('#app');
+  app.mount("#app");
+
+  // Inicializar OneSignal
+  OneSignal.initialize("ea3369eb-bd7b-4c01-8068-55bceb4e89b7");
+  OneSignal.Notifications.requestPermission();
+
+ 
+  const interval = setInterval(async () => {
+    const id = await OneSignal.User.getOnesignalId();
+    console.log("OneSignal ID:", id);
+
+    if (id) {
+      clearInterval(interval);
+    }
+  }, 60000);
+
+ 
+  const displayNotification = async (event: NotificationWillDisplayEvent) => {
+    const notification = event.getNotification();
+    event.getNotification().display(); 
+
+    console.log("Notificación recibida:", notification);
+
+    
+    const alertOptions: AlertOptions = {
+      header: notification.title,
+      message: notification.body,
+      buttons: ["OK"],
+    };
+    const alert = await alertController.create(alertOptions);
+
+    await alert.present();
+  };
+
+  OneSignal.Notifications.addEventListener("foregroundWillDisplay", displayNotification);
+
+
+  const openNotification = async (event: NotificationClickEvent) => {
+    const notification = event.notification;
+    console.log("Notificación clickeada:", notification);
+
+   
+    const alert = await alertController.create({
+      header: notification.title,
+      message: notification.body, 
+      buttons: ["OK"],
+    });
+
+    await alert.present();
+  };
+
+  OneSignal.Notifications.addEventListener("click", openNotification);
 });
